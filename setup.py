d83b2738f07db372780b700ae25defd7b89b8b15
Leon Oostrum <leonoostrum@gmail.com>
2019-01-14 15:25:23 +0100

    Add setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='darc',
+      version='0.1',
+      description='Data Analysis of Real-time Candidates from ARTS',
+      url='http://github.com/loostrum/darc',
+      author='Leon Oostrum',
+      author_email='oostrum@astron.nl',
+      license='GPLv3',
+      packages=['darc'],
+      zip_safe=False)
