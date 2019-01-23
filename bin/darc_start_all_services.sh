b72350ffa582610eaeca1673256080e83d1ab6ab
Leon Oostrum <leonoostrum@gmail.com>
2019-01-23 16:02:22 +0100

    Working AMBER listener and triggering services. Added master service. Communication through control.py

--- bin/darc_start_all_services.sh
+++ bin/darc_start_all_services.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#
-# start as "nohup darc_start_all_services.sh"
+# 
+# Start all DARC services
 
-# AMBER trigger listener
+# Master service
+if ps ax | grep "[d]arc_service" > /dev/null; then
+    echo "DARC service online"
+else
+    echo "DARC service offline, starting"
+    darc_service 2>&1 &
+    sleep 1
+fi
 
-darc --service amber_listener &
+# start services
+echo "Starting AMBER Listener"
+darc --service amber_listener --cmd start
+echo "Starting AMBER Triggering"
+darc --service amber_triggering --cmd start
