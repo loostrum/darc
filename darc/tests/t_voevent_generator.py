5ce179c8e209e7f355dca85f588f7c7419e51080
Leon Oostrum <leonoostrum@gmail.com>
2019-01-24 17:46:57 +0100

    Working voevent generator

--- darc/tests/t_voevent_generator.py
+++ darc/tests/t_voevent_generator.py
@@ -10,6 +10,7 @@ from darc.voevent_generator import VOEventGenerator
 
 
 if __name__ == '__main__':
+    print 'Initalizing'
     # create a queue
     queue = Queue()
     # create stop event for generator
@@ -19,7 +20,18 @@ if __name__ == '__main__':
     # set the queue
     generator.set_source_queue(queue)
     # start the generator
+    print 'Starting generator'
     generator.start()
     # create a trigger
-    trigger = "trigger"
+    trigger = {'dm': 56.791, 'dm_err': .2, 'width': 2.5, 'snr': 25, 'flux': 0.5,
+               'ra': 83.63322083333333, 'dec': 22.01446111111111, 'ymw16': 0,
+               'semiMaj': 15., 'semiMin': 15., 'name': 'B0531+21', 
+               'importance': 0.1, 'utc': '2019-01-01-18:00:00.0'}
+    print 'Sending trigger'
     queue.put(trigger)
+    # wait for a while
+    print 'Sleeping'
+    sleep(5)
+    # stop the generator
+    print 'Stopping generator'
+    stop_event.set()
--- darc/voevent_generator.py
+++ darc/voevent_generator.py
@@ -2,18 +2,21 @@
 #
 # Logic for listening on a network port
 
-from time import sleep
+import ast
+import errno
 import os
 import sys
-import socket
 import yaml
 import logging
 import logging.handlers
 from time import sleep, time
 from queue import Queue, Empty
 import threading
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 from darc.definitions import *
+from darc.VOEvent_create import NewVOEvent
 
 
 class VOEventGeneratorException(Exception):
@@ -50,6 +53,15 @@ class VOEventGenerator(threading.Thread):
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(handler)
 
+        # create and cd to voevent directory
+        try:
+            os.makedirs(self.voevent_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                self.logger.error("Cannot create voevent directory:, {}".format(e))
+                raise VOEventGeneratorException("Cannot create voevent directory")
+        os.chdir(self.voevent_dir)
+
     def set_source_queue(self, queue):
         if not isinstance(queue, Queue):
             self.logger.error('Given source queue is not an instance of Queue')
@@ -61,7 +73,38 @@ class VOEventGenerator(threading.Thread):
             self.logger.error('Queue not set')
             raise VOEventGeneratorException('Queue not set')
 
-        self.logger.info("Running VOEvent dummy")
+        self.logger.info("Running VOEvent generator")
         while not self.stop_event.is_set():
-            self.stop_event.wait(timeout=5)
-        self.logger.info("Stopping VOEvent dummy")
+            self.stop_event.wait(timeout=1)
+            try:
+                trigger = self.voevent_queue.get_nowait()
+            except Empty:
+                continue
+            self.logger.info("Received trigger: {}".format(trigger))
+            self.create_and_send(trigger)
+
+        self.logger.info("Stopping VOEvent generator")
+
+    def create_and_send(self, trigger):
+        if not isinstance(trigger, dict):
+            self.logger.error("Trigger is not a dict")
+            return
+
+        # check if all required params are present
+        # gl and gb are generated from ra and dec
+        # semiMaj and semiMin have default set for IAB
+        keys = ['dm', 'dm_err', 'width', 'snr', 'flux', 'semiMaj', 'semiMin',
+                'ra', 'dec', 'ymw16', 'name', 'importance', 'utc']
+        for key in keys:
+            if key not in trigger.keys():
+                self.logger.error("Parameter missing from trigger: {}".format(key))
+                return
+
+        # Parse coordinates
+        coord = SkyCoord(ra=trigger['ra']*u.degree, dec=trigger['dec']*u.degree, frame='icrs')
+        trigger['gl'] = coord.galactic.l.deg
+        trigger['gb'] = coord.galactic.b.deg
+
+        self.logger.info("Creating VOEvent")
+        NewVOEvent(**trigger)
+        self.logger.info("Generated VOEvent")
