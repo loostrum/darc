9d08d24f6bd8b206354bd8fe37c8ce30ae056cfc
Leon Oostrum <leonoostrum@gmail.com>
2019-03-25 17:40:20 +0100

    Moved process_triggers.sh into observation_control. Not tested yet

--- darc/observation_control.py
+++ darc/observation_control.py
@@ -3,6 +3,7 @@
 # DARC master process
 # Controls all services
 
+import os
 import sys
 import ast
 import yaml
@@ -10,19 +11,22 @@ import errno
 import multiprocessing as mp
 import threading
 import socket
+import subprocess
 
 from darc.definitions import *
 from darc.logger import get_logger
 from darc.control import send_command
+from darc import util
 
 
 class ObservationControlException(Exception):
     pass
 
 class ObservationControl(threading.Thread):
-    def __init__(self, obs_config, host_type):
+    def __init__(self, obs_config, host_type, stop_event):
         threading.Thread.__init__(self)
         self.daemon = True
+        self.stop_event = stop_event
 
         with open(CONFIG_FILE, 'r') as f:
             config = yaml.load(f)['observation_control']
@@ -33,12 +37,15 @@ class ObservationControl(threading.Thread):
             if isinstance(value, str):
                 value = value.format(**kwargs)
             setattr(self, key, value)
+        self.config = config
 
         # setup logger
         self.logger = get_logger(__name__, self.log_file)
 
-        # store observation config and host type
-        self.obs_config = obs_config
+        # store general and observation-specific settings in obs_config
+        self.obs_config = config.copy()
+        self.obs_config.update(obs_config)
+        # save whether we should run worker or master observations
         self.host_type = host_type
 
         self.logger.info('ObservationControl initialized')
@@ -77,23 +84,136 @@ class ObservationControl(threading.Thread):
         #for service in ['amber_listener']:
         #    send_command(self.timeout, service, 'start')
 
+        # TAB or IAB mode
+        if self.obs_config['ntabs'] == 1:
+            self.mode = 'IAB'
+        else:
+            self.mode = 'TAB'
 
-        # start old-type processing: process_triggers.sh
-        process_trigger_script = '{home}/ARTS-obs/process_triggers.sh'.format(home=os.path.expanduser('~'))
-        # Loop over 12 TABs, or single IAB
-        #for tab in range(1, self.obs_config['ntabs']+1):
-        for tab in [1]:
-            if self.obs_config['ntabs'] == 1:
-                self.obs_config['fil_suffix'] = ".fil"
-                tab=0
+        # wait until end time + 10s
+        start_processing_time = Time(self.obs_config['endtime']) + TimeDelta(10, format='sec')
+        self.logger.info("Sleeping until {}".format(start_processing_time))
+        util.sleepuntil_utc(start_processing_time, event=self.stop_event)
+
+        # merge the trigger files
+        self.logger.info("Merging raw trigger files")
+        numcand_raw = self._merge_triggers()
+
+        # Run clustering for IAB / each TAB
+        self.logger.info("Clustering candidates")
+        numcand_grouped = 0
+        # ToDo run in parallel
+        for tab in range(self.obs_config['numtabs']):
+            if self.mode == 'IAB':
+                filterbank_file = "{output_dir}/filterbank/CB{beam:02d}.fil".format(**self.obs_config)
             else:
-                self.obs_config['fil_suffix'] = "_{:02d}.fil".format(tab)
+                filterbank_file = "{output_dir}/filterbank/CB{beam:02d}_{tab:02d}.fil".format(tab=tab+1, **self.obs_config)
+            numcand_grouped += self._cluster(tab, filterbank_file)
 
-            cmd = "(sleepuntil_utc {endtime}; sleep 10; " \
-                  "{process_trigger_script} {output_dir}/triggers {output_dir}/filterbank/CB{beam:02d}{fil_suffix} " \
-                  "{amber_dir}/CB{beam:02d} {master_dir} " \
-                  "{snrmin_processing} {snrmin_processing_local} {dmmin} {dmmax} {beam:02d} {duration} {tab:02d}) 2>&1 > {output_dir}/triggers/process_triggers.log &".format(process_trigger_script=process_trigger_script, tab=tab,
-                                                              **self.obs_config)
+        # Run classifier
+        self.logger.info("Classifying candidates")
+        output_prefix, data_file = self._classify()
+        
+        # Gather results
+        self.logger.info("Gathering results")
+        kwargs = {'output_prefix': output_prefix, 'data_file': data_file, 'numcand_raw': numcand_raw, 'numcand_grouped': numcand_grouped}
+        self._gather_results(kwargs)
 
+        return
+
+        def _merge_triggers(self):
+            """
+            Merge AMBER triggers into one file
+            """
+            prefix = "{amber_dir}/CB{beam:02d}".format(**self.obs_config)
+            cmd = "awk '(FNR==1 && NR!=1) || !/./{{next;}}{{print}}' {prefix}_step*.trigger > {prefix}.trigger".format(prefix=prefix)
+            # awk command from Thijs:
+            # (FNR==1 && NR!=1) means "if it's the first line of the current file we're processing, 
+            # but not the first line read overall then skip this line {"next"}. This matches the first 
+            # header line in all files expect the first file. The other clause "|| !/./"  makes it skip
+            # empty lines, which can happen when there is superfluous carriage returns at the end of files.
             self.logger.info("Running {}".format(cmd))
             os.system(cmd)
+
+            # check number of triggers
+            cmd = "grep -v \# {prefix}.trigger | wc -l".format(prefix=prefix)
+            try:
+                numcand_raw = int(subprocess.check_output(cmd)) - 1
+            except Exception as e:
+                self.logger.warning("Failed to get number of raw triggers, setting to -1: {}".format(e))
+                numcand_raw = -1
+
+            return numcand_raw
+
+        def _cluster(self, tab, filterbank_file):
+            """
+            Run triggers.py
+            :param: tab: TAB number to process (0 for IAB)
+            :params: filterbank_file: Full path to filterbank file to use
+            """
+
+            prefix = "{amber_dir}/CB{beam:02d}".format(**self.obs_config)
+            cmd = "python {triggering} --rficlean --sig_thresh_local {snrmin_processing_local} --time_limit {duration} --descending_snr " \
+                  " --beamno {beam:02d} --mk_plot --dm_min {dmmin} --dm_max {dmmax} --sig_thresh {snrmin_processing} --ndm {ndm} " \
+                  " --save_data concat --nfreq_plot {nfreq_plot} --ntime_plot {ntime_plot} --cmap {cmap} --outdir={output_dir}/triggers " \
+                  " --tab {tab} {filterbank_file} {prefix}.trigger".format(tab=tab, filterbank_file=filterbank_file, prefix=prefix, **self.obs_config)
+            self.logger.info("Running {}".format(cmd))
+            os.system(cmd)
+
+            # Check number of candidates
+            if self.mode == 'IAB':
+                fname = '{output_dir}/triggers/grouped_pulses.singlepulse'.format(**self.obs_config)
+            else:
+                fname = '{output_dir}/triggers/grouped_pulses_{tab:02d}.singlepulse'.format(tab=tab+1, **self.obs_config)
+
+            cmd = "wc -l {fname} | awk '{{print $1}}')".format(fname=fname)
+            try:
+                numcand_grouped = int(subprocess.check_output(cmd))
+            except Exception as e:
+                self.logger.warning("Failed to get number of grouped triggers, setting to -1: {}".format(e))
+                numcand_grouped = -1
+
+            return numcand_grouped
+
+
+        def _classify(self):
+            """
+            Run the ML classifier in python 3 virtualenv
+            """
+            if self.mode == 'IAB':
+                output_prefix = "{output_dir}/triggers/ranked_CB{beam:02d}".format(**self.obs_config)
+                data_file = '{output_dir}/triggers/data/data_full.hdf5'.format(**self.obs_config)
+            else:
+                output_prefix = "{output_dir}/triggers/ranked_CB{beam:02d}_TAB{:02d}".format(tab=tab, **self.obs_config)
+                data_file =  '{output_dir}/triggers/data/data_{tab:02d}_full.hdf5'.format(tab=tab+1,**self.obs_config)
+
+            cmd = "source {venv_dir}/bin/activate; export CUDA_VISIBLE_DEVICES={ml_gpus}; python {classifier} " \
+                  " --fn_model_time {model_dir}/heimdall_b0329_mix_147411d_time.hdf5 " \
+                  " --pthresh {pthresh} --save_ranked --plot_ranked --fnout={output_prefix} {data_file} " \
+                  " {model_dir}/20190125-17114-freqtimefreq_time_model.hdf5".format(output_prefix=output_prefix, **self.obs_config)
+            self.logger.info("Running {}".format(cmd))
+            os.system(cmd)
+            #ToDo: count number of output figures
+            return output_prefix, data_file
+
+        def _gather_results(self, **kwargs):
+            """
+            Merge classifier output plots and call trigger_to_master
+            :param: kwargs: number of candidate, output prefixes
+            """
+
+            # Merge output figures
+            output_file = "{output_dir}/triggers/candidates_summary.pdf".format(**self.obs_config)
+            cmd = "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile={output_file} {output_dir}/triggers/*pdf".format(**self.obs_config)
+            self.logger.info("Running {}".format(cmd))
+            os.system(cmd)
+
+            # Copy results
+            # ToDo: properly get results of all TABs
+            conf = self.obs_config.copy()
+            conf.update(**kwargs)
+            cmd = "python {trigger_to_master} {data_file} {output_prefix}_freq_time.hdf5 {numcand_raw} {numcand_grouped} {master_dir}".formmat(**conf)
+            self.logger.info("Running {}".format(cmd))
+            os.system(cmd)
+    
+            return
