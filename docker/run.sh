33cf18e3048538bdc07deb112e240c68b6d2ecd6
Leon Oostrum <leonoostrum@gmail.com>
2019-04-26 16:31:09 +0200

    Can't use actual arts hostnames when running docker on arts041

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
-docker run -itd --network apertif --name arts041 -h arts041 -p 8080:80 arts 
-docker run -itd --network apertif --name arts001 -h arts001 arts 
-docker run -itd --network apertif --name control -h control arts
+docker run -itd --name master -h master -p 8080:80 arts 
+docker run -itd --name worker -h worker arts 
+docker run -itd --name control -h control arts
