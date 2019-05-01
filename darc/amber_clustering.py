e4d5ff53be7999e2d4a32fc95b05d218bde5deb8
Leon Oostrum <leonoostrum@gmail.com>
2019-05-01 15:46:26 +0200

    First version of amber clustering (to replace amber trigger - that should only be used for direct VO/IQUV triggers from AMBER raw triggers

---
+++ darc/amber_clustering.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+#
+# AMBER Clustering
+
+import yaml
+from time import time
+import multiprocessing as mp
+try:
+    from queue import Empty
+except ImportError:
+    from Queue import Empty
+import threading
+import socket
+import numpy as np
+
+from darc.definitions import *
+from darc.logger import get_logger
+from darc.external import tools
+
+
+class AMBERClusteringException(Exception):
+    pass
+
+
+class AMBERClustering(threading.Thread):
+    """
+    Cluster AMBER clusters
+    """
+
+    def __init__(self, stop_event):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.stop_event = stop_event
+
+        self.amber_queue = None
+        self.cluster_queue = None
+
+        self.hdr_mapping = {}
+        self.start_time = None
+
+        with open(CONFIG_FILE, 'r') as f:
+            config = yaml.load(f, Loader=yaml.SafeLoader)['amber_clustering']
+
+        # set config, expanding strings
+        kwargs = {'home': os.path.expanduser('~'), 'hostname': socket.gethostname()}
+        for key, value in config.items():
+            if isinstance(value, str):
+                value = value.format(**kwargs)
+            setattr(self, key, value)
+
+        # setup logger
+        self.logger = get_logger(__name__, self.log_file)
+        self.logger.info("AMBER Clustering initialized")
+
+    def set_source_queue(self, queue):
+        """
+        :param queue: Source of amber clusters
+        """
+        if not isinstance(queue, mp.queues.Queue):
+            self.logger.error('Given source queue is not an instance of Queue')
+            raise AMBERClusteringException('Given source queue is not an instance of Queue')
+        self.amber_queue = queue
+
+    def set_target_queue(self, queue):
+        """
+        :param queue: Output queue for clusters
+        """
+        if not isinstance(queue, mp.queues.Queue):
+            self.logger.error('Given target queue is not an instance of Queue')
+            raise AMBERClusteringException('Given target queue is not an instance of Queue')
+        self.cluster_queue = queue
+
+    def run(self):
+        if not self.amber_queue:
+            self.logger.error('AMBER cluster queue not set')
+            raise AMBERClusteringException('AMBER cluster queue not set')
+        if not self.cluster_queue:
+            self.logger.error('Cluster queue not set')
+            raise AMBERClusteringException('Cluster queue not set')
+
+        self.logger.info("Starting AMBER clustering")
+        while not self.stop_event.is_set():
+            # read clusters for _interval_ seconds
+            clusters = []
+            tstart = time()
+            curr_time = tstart
+            while curr_time < tstart + self.interval and not self.stop_event.is_set():
+                curr_time = time()
+                try:
+                    data = self.amber_queue.get(timeout=.1)
+                except Empty:
+                    continue
+
+                if isinstance(data, str):
+                    clusters.append(data)
+                elif isinstance(data, list):
+                    clusters.extend(data)
+
+            # start processing in thread
+            if clusters:
+                proc_thread = threading.Thread(target=self.process_clusters, args=[clusters])
+                proc_thread.daemon = True
+                proc_thread.start()
+            else:
+                self.logger.info("No clusters")
+        self.logger.info("Stopping AMBER clustering")
+            
+
+    def process_clusters(self, clusters):
+        """
+        Applies thresholding to clusters
+        Put approved clusters on queue
+        :param clusters: list of clusters to process
+        """
+        self.logger.info("Starting processing of {} clusters".format(len(clusters)))
+        # check for header
+        if not self.hdr_mapping:
+            self.logger.info("Checking for header")
+            for cluster in clusters:
+                if cluster.startswith('#'):
+                    # TEMP: set observation start time to now
+                    self.start_time = time()
+                    # read header, remove comment symbol
+                    header = cluster.split()[1:]
+                    self.logger.info("Received header: {}".format(header))
+                    # Check if all required params are present and create mapping to col index
+                    keys = ['beam_id', 'integration_step', 'time', 'DM', 'SNR']
+                    for key in keys:
+                        try:
+                            self.hdr_mapping[key] = header.index(key)
+                        except ValueError:
+                            self.logger.error("Key missing from clusters header: {}".format(key))
+                            self.hdr_mapping = {}
+                            return
+                    # remove header from clusters
+                    clusters.remove(cluster)
+                    # clusters is now empty if only header was received
+                    if not clusters:
+                        self.logger.info("Only header received - Canceling processing")
+                        return
+                    else:
+                        break
+
+        if not self.hdr_mapping:
+            self.logger.error("First clusters received but header not found")
+            return
+
+
+        # split strings
+        clusters = np.array(list(map(lambda val: val.split(), clusters)), dtype=float)
+
+        self.logger.info("Clustering")
+        clusters_for_clustering = clusters[:, (self.hdr_mapping['DM'], self.hdr_mapping['SNR'], self.hdr_mapping['time'], self.hdr_mapping['integration_step'])]
+
+        # ToDo: feed other obs parameters
+        cluster_snr, cluster_dm, cluster_time, cluster_downsamp, _ = tools.get_triggers(clusters_for_clustering, tab=clusters[:, self.hdr_mapping['beam_id']])
+        self.logger.info("Clustering done")
+        # put clusters on queue
+        clusters = np.transpose([cluster_snr, cluster_dm, cluster_time, cluster_downsamp])
+        columns = {'SNR': 0, 'DM':1, 'time': 2, 'integration_step': 3}
+        self.cluster_queue.put({'clusters': clusters, 'columns': columns})
+
+        #print("Generating VO for highest S/N")
+        #ind = np.argmax(cluster_snr)
+        #voevent_cluster = {'dm': cluster_dm[ind], 'dm_err': 0,
+        #                  'width': cluster_downsamp[ind]*81.92E-3,
+        #                  'snr': cluster_snr[ind], 'flux': 0,
+        #                  'ra': 83.63322083333333, 'dec': 22.01446111111111,
+        #                  'ymw16': 0, 'semiMaj': 15., 'semiMin': 15., 'name': 'B0531+21',
+        #                  'importance': 0.1, 'utc': '2019-01-01-18:00:00.0'}
+        #self.logger.info("Putting cluster on voevent queue: {}".format(voevent_cluster))
+        #self.cluster_queue.put(voevent_cluster)
+
+
---
+++ darc/tests/t_amber_clustering.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+import multiprocessing as mp
+import threading
+from time import sleep
+import numpy as np
+try:
+    from queue import Empty
+except ImportError:
+    from Queue import Empty
+
+from darc.amber_clustering import AMBERClustering
+
+
+class TestAMBERClustering(unittest.TestCase):
+
+    def test_clustering(self):
+        """
+        Test AMBER Clustering
+        """
+
+        # create queues
+        in_queue = mp.Queue()
+        out_queue = mp.Queue()
+        # create stop event for clustering
+        stop_event = threading.Event()
+        # init AMBER Clustering
+        clustering = AMBERClustering(stop_event)
+        # set the queues
+        clustering.set_source_queue(in_queue)
+        clustering.set_target_queue(out_queue)
+        # start the clustering
+        clustering.start()
+
+        # load triggers to put on queue
+        nline_to_check = 50
+        trigger_file = 'CB00_step1.trigger'
+        # check the output is correct, i.e. equal to input
+        line = 0 
+        with open(trigger_file, 'r') as f:
+            triggers = f.readlines()
+        if len(triggers) > nline_to_check:
+            triggers = triggers[:nline_to_check]
+        triggers = [line.strip() for line in triggers]
+
+        # put triggers on queue
+        in_queue.put(triggers)
+        # get output
+        sleep(clustering.interval + 5)
+        try:
+            output = out_queue.get(timeout=5)
+        except Empty:
+            output = []
+
+        expected_output = {'clusters': np.array([[1.84666e+01, 5.66000e+01, 2.44941e-02, 1.00000e+00],
+                            [1.03451e+01, 8.00000e+01, 9.01120e-01, 1.00000e+03],
+                            [1.08202e+01, 4.78000e+01, 2.94912e+00, 1.00000e+03],
+                            [1.43720e+01, 4.16000e+01, 9.01120e-01, 1.00000e+03],
+                            [1.57447e+01, 1.58000e+01, 2.94912e+00, 1.00000e+03]]), 
+                            'columns': {'SNR': 0, 'DM': 1, 'time': 2, 'integration_step': 3}}
+        
+        # test columns are equal
+        self.assertDictEqual(output['columns'], expected_output['columns'])
+        # test clusters are equal
+        np.testing.assert_array_equal(output['clusters'], expected_output['clusters'])
+
+if __name__ == '__main__':
+    unittest.main()
