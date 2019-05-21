b7131b9328b4bfe34c1fb211a0f29aa6a3fcd0e1
Leon Oostrum <leonoostrum@gmail.com>
2019-05-21 09:45:54 +0200

    Revert "Proper start when service stopped itself"
    This reverts commit ac5343d59bf914b7429fe3581e203b3297ba3933.

--- darc/darc_master.py
+++ darc/darc_master.py
@@ -305,17 +305,9 @@ class DARCMaster(object):
         self.logger.info("Starting service: {}".format(service))
         # check if already running
         if thread.isAlive():
-            if thread.exitcode is not None:
-                # was running but stopped
-                thread.join()
-                self.logger.info("Creating new thread for service {}".format(service))
-                self.create_thread(service)
-                thread = self.threads[service]
-            else:
-                # already running
-                status = 'Success'
-                reply = "Service already running: {}".format(service)
-                self.logger.warning(status)
+            status = 'Success'
+            reply = "Service already running: {}".format(service)
+            self.logger.warning(status)
         else:
             # set queues
             if source_queue:
