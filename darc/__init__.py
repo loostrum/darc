4a11628724c10a18958f7b3db6b73f6bc2729c46
Leon Oostrum <leonoostrum@gmail.com>
2019-09-03 18:24:06 +0200

    Add init for easier import in external scripts

---
+++ darc/__init__.py
@@ -0,0 +1,17 @@
+# Initialization
+from amber_listener import AMBERListener
+from amber_clustering import AMBERClustering
+from amber_triggering import AMBERTriggering
+from dada_trigger import DADATrigger
+from offline_processing import OfflineProcessing
+from processor import Processor
+from sb_generator import SBGenerator
+from status_website import StatusWebsite
+from voevent_generator import VOEventGenerator
+
+from logger import get_logger
+
+
+__all__ = ['AMBERListener', 'AMBERClustering', 'AMBERTriggering', 'DADATrigger',
+           'OfflineProcessing', 'Processor', 'SBGenerator', 'StatusWebsite',
+           'VOEventGenerator', 'get_logger']
