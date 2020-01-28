01f2e8c743fd9679095746cd97590c482a41133f
Leon Oostrum <leonoostrum@gmail.com>
2020-01-28 10:59:58 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='darc',
-      version='1.0.1',
+      version='1.0.3',
       description='Data Analysis of Real-time Candidates from ARTS',
       url='http://github.com/loostrum/darc',
       author='Leon Oostrum',
