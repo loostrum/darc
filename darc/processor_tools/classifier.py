Attempt at fixing data scaling for classifier/visualizer

darc/processor_tools/classifier.py
@@ -206,16 +206,19 @@ class Classifier(threading.Thread):
             self.data_dm_time = self.data_dm_time.reshape(self.config.ndm,
                                                           self.config.ntime, modulo).mean(axis=2)
 
-        # scale data
+        # scale data and add required axis for classifier
+        badchan = self.data_freq_time.sum(axis=1) == 0
         self.data_freq_time -= np.median(self.data_freq_time, axis=-1, keepdims=True)
-        self.data_dm_time -= np.median(self.data_dm_time, axis=-1, keepdims=True)
+        # ensure bad channels are still zero
+        self.data_freq_time[badchan] = 0
+
         # silence the potential runtime warning due to divide-by-zero
         with np.errstate(invalid='ignore'):
             self.data_freq_time /= np.std(self.data_freq_time, axis=-1, keepdims=True)
-            self.data_dm_time /= np.std(self.data_dm_time, axis=-1, keepdims=True)
-        # note: for classifier do not replace non-finite numbers by zero
-
-        # add required axes for classifier
         self.data_freq_time = self.data_freq_time[None, ..., None]
+
+        self.data_dm_time -= np.median(self.data_dm_time, axis=-1, keepdims=True)
+        self.data_dm_time /= np.std(self.data_dm_time, axis=-1, keepdims=True)
         self.data_dm_time = self.data_dm_time[None, ..., None]
+
         return True

darc/processor_tools/visualizer.py
@@ -235,7 +235,10 @@ class Visualizer:
             # load and scale the data
             if data_type == 'freq_time':
                 data = f['data_freq_time'][:]
+                badchan = data.sum(axis=1) == 0
                 data -= np.median(data)
+                # ensure bad channels are still zero
+                data[badchan] = 0
                 # silence the potential runtime warning due to divide-by-zero
                 with np.errstate(invalid='ignore'):
                     data /= np.std(data, axis=1, keepdims=True)
@@ -243,10 +246,6 @@ class Visualizer:
             elif data_type == 'dm_time':
                 data = f['data_dm_time'][:]
                 data -= np.median(data)
-                # silence the potential runtime warning due to divide-by-zero
-                with np.errstate(divide='ignore'):
-                    data /= np.std(data, axis=1, keepdims=True)
-                data[~np.isfinite(data)] = np.nan
             elif data_type == '1d_time':
                 data = f['data_freq_time'][:].sum(axis=0)
                 data -= np.median(data)