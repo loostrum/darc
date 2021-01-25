61bce958a7c9337c68e82d05cbfea8a98aee43a6
Leon Oostrum <leonoostrum@gmail.com>
2021-01-25 16:42:20 +0100

    Scale classifier data only by global mean/stdev instead of per-channel; remove old debug line from clustering

--- darc/processor_tools/classifier.py
+++ darc/processor_tools/classifier.py
@@ -238,15 +238,15 @@ class Classifier(mp.Process):
                                                           self.config.ntime, modulo).mean(axis=2)
 
         # scale data and add required axis for classifier
-        self.data_freq_time -= np.median(self.data_freq_time, axis=1, keepdims=True)
+        self.data_freq_time -= np.median(self.data_freq_time)
         # silence the potential runtime warning due to divide-by-zero
         with np.errstate(invalid='ignore'):
-            self.data_freq_time /= np.std(self.data_freq_time, axis=1, keepdims=True)
+            self.data_freq_time /= np.std(self.data_freq_time)
         self.data_freq_time[np.isnan(self.data_freq_time)] = 0.
         self.data_freq_time = self.data_freq_time[None, ..., None]
 
-        self.data_dm_time -= np.median(self.data_dm_time, axis=1, keepdims=True)
-        self.data_dm_time /= np.std(self.data_dm_time, axis=1, keepdims=True)
+        self.data_dm_time -= np.median(self.data_dm_time)
+        self.data_dm_time /= np.std(self.data_dm_time)
         self.data_dm_time = self.data_dm_time[None, ..., None]
 
         return True
