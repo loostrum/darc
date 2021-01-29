33fe5cfb95799ba00cbccfe1b66d95e2635cf91b
Leon Oostrum <leonoostrum@gmail.com>
2021-01-29 12:38:42 +0100

    Add orange border to galactic candidates

--- darc/processor_tools/visualizer.py
+++ darc/processor_tools/visualizer.py
@@ -182,6 +182,9 @@ class Visualizer:
                     # add green border if DM > DMgal
                     if params['dm'] > dmgal:
                         plt.setp(ax.spines.values(), color='green', linewidth=2, alpha=0.85)
+                    # else make it orange
+                    else:
+                        plt.setp(ax.spines.values(), color='orange', linewidth=2, alpha=0.85)
 
                     # on the last page, disable the remaining plots if there are any
                     if page == npage - 1:
