1d1747b6874fab07a3d2e744f0c744269cbdc0e3
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-11-21 10:16:54 +0100

    Style fix

--- darc/external/tools.py
+++ darc/external/tools.py
@@ -351,12 +351,12 @@ def get_triggers(fn, sig_thresh=5.0, dm_min=0, dm_max=np.inf,
     else:
         beam_amber = None
 
-    if type(fn) == str:
+    if isinstance(fn, str):
         if read_beam:
             dm, sig, tt, downsample, beam = read_singlepulse(fn, max_rows=max_rows, beam=beam_amber)[:5]
         else:
             dm, sig, tt, downsample = read_singlepulse(fn, max_rows=max_rows, beam=beam_amber)[:4]
-    elif type(fn) == np.ndarray:
+    elif isinstance(fn, np.ndarray):
         if read_beam:
             dm, sig, tt, downsample, beam = fn[:, 0], fn[:, 1], fn[:, 2], fn[:, 3], fn[:, 4]
         else:
